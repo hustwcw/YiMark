5ab15b098985cb62bd129be0d1a176356a2651ff
tck <tck@tckdeMac-mini.local>
2014-05-17 16:16:23 +0800

    modify shell

--- archive.sh
+++ archive.sh
@@ -8,10 +8,11 @@ fi
 sed  -i -e "/CFBundleIdentifier/{n; s/\(<string>\).*\(<\/string>\)/\1com.yimark.$1\2/; }" ./YiMark/YiMark-Info.plist
 sed  -i -e "/CFBundleDisplayName/{n; s/\(<string>\).*\(<\/string>\)/\1$2\2/; }" ./YiMark/YiMark-Info.plist
 #/usr/libexec/plistbuddy -c "SET :CFBundleDisplayName "$2"" ./YiMark/YiMark-Info.plist
-#sed  -i -e "/Website/{n; s/\(<string>\).*\(<\/string>\)/\1$3\2/; }" ./YiMark/test.plist
-/usr/libexec/plistbuddy -c "SET :Website '$3'" ./YiMark/test.plist
+#sed  -i -e "/Website/{n; s/\(<string>\).*\(<\/string>\)/\1$3\2/; }" ./YiMark/config.plist
+/usr/libexec/plistbuddy -c "SET :Website '$4'" ./YiMark/config.plist
 
 # 
 srcDir=`pwd`
+curl -o Icon.png $3
 xcodebuild -target YiMark -sdk iphoneos build
 xcrun -sdk iphoneos PackageApplication -v "build/Release-iphoneos/YiMark.app" -o "$srcDir/build/release/$1.ipa" --sign "hangzhou caizhu network technology co., LTD." --embed "Inhouse_Distribution.mobileprovision"
